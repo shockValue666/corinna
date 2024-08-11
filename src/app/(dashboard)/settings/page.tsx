e33b89ce9bc80e867a04d9e666ce818eef61b83b
shockValue666 <mapedeveloper@protonmail.com>
2024-08-11 16:50:42 -0700

    more

--- src/app/(dashboard)/settings/page.tsx
+++ src/app/(dashboard)/settings/page.tsx
@@ -11,7 +11,7 @@ const Page = () => {
     <>
       <InfoBar/>
       <div className='overflow-y-auto w-full chat-window flex-1 h-0 flex flex-col gap-10'>
-        <BillingSettings/>
+        {/* <BillingSettings/> */}
         <DarkModeToggle/>
         <ChangePassword/>
       </div>
